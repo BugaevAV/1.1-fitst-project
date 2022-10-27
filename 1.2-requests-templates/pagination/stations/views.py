0ac6cea381c855fab164c4be41644cd0a9d4ae8f
BugaevAV <iustusvir@gmail.com>
2022-10-27 06:16:41 +0300

    pagination csv file

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,3 +1,6 @@
+import csv
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -8,10 +11,15 @@ def index(request):
 
 def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
+    page_num = int(request.GET.get('page', 1))
     # также передайте в контекст список станций на странице
-
-    context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
-    }
-    return render(request, 'stations/index.html', context)
+    with open('data-398-2018-08-30.csv', encoding='utf-8', newline='') as f:
+        reader = csv.DictReader(f)
+        list_stations = [row for row in reader]
+        paginator = Paginator(list_stations, 8)  # 1351 страница
+        page = paginator.get_page(page_num)
+        context = {
+            'bus_stations': page,
+            'page': page,
+        }
+        return render(request, 'stations/index.html', context)
