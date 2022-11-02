973308ba471d66e869fd3c891f5c2bb984e29458
BugaevAV <iustusvir@gmail.com>
2022-11-02 21:05:51 +0300

    create and filling database, adding info from csv to the db through the terminal, sorting functions

--- 2.1-databases/work_with_database/phones/admin.py
+++ 2.1-databases/work_with_database/phones/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'slug', 'image', 'price', 'release_date', 'lte_exists']
+    prepopulated_fields = {'slug': ('name',)}
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
 
 
 def show_catalog(request):
+    sorting = request.GET.get('sort')
+    if sorting == 'name':
+        phone_obj = Phone.objects.order_by('name')
+    elif sorting == 'min_price':
+        phone_obj = Phone.objects.order_by('price')
+    elif sorting == 'max_price':
+        phone_obj = Phone.objects.order_by('price').reverse()
+    else:
+        phone_obj = Phone.objects.all()
     template = 'catalog.html'
-    context = {}
+    context = {'phones': phone_obj}
     return render(request, template, context)
 
 
 def show_product(request, slug):
+    phone_obj = Phone.objects.filter(slug=slug)[0]
+    print(phone_obj)
     template = 'product.html'
-    context = {}
+    context = {'phone': phone_obj}
     return render(request, template, context)
