3d4620fe76a2cbd882599001b425b976286da84e
BugaevAV <iustusvir@gmail.com>
2022-11-14 17:38:56 +0300

    Articles: settings the admin site, sorting of tags

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, ArticleScopes
+
+
+class ArticleScopesInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count_true = [form.cleaned_data.get('is_main') for form in self.forms].count(True)
+        if count_true < 1:
+            raise ValidationError('Укажите основной раздел')
+        elif count_true > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        return super().clean()
+
+
+class ArticleScopesInline(admin.TabularInline):
+    model = ArticleScopes
+    extra = 3
+    formset = ArticleScopesInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ArticleScopesInline]
+
+
+@admin.register(Scope)
+class ScopesAdmin(admin.ModelAdmin):
     pass
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -6,11 +6,38 @@ class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    articles = models.ManyToManyField(Article, through='ArticleScopes', verbose_name='Выбор статьи')
+    name = models.CharField(max_length=200, verbose_name='Имя тега')
+
+    def __str__(self):
+        return self.name
+
+
+class ArticleScopes(models.Model):
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = ' Тематики статьи'
+        ordering = ['-is_main', 'tag']
+
+    article = models.ForeignKey(Article, related_name='scopes', on_delete=models.CASCADE)
+    tag = models.ForeignKey(Scope, related_name='scopes', on_delete=models.CASCADE, verbose_name='РАЗДЕЛ')
+    is_main = models.BooleanField(verbose_name='ОСНОВНОЙ', )
+
